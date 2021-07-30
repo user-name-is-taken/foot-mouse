02cc19068f8cf1aaeff7093850e83c15e4fe0cd1
michael lundquist <michaellundquist7@gmail.com>
2021-07-30 15:31:37 -0400

    fixing how I get the accelerometer value

--- footMouse/footMouse.ino
+++ footMouse/footMouse.ino
@@ -13,27 +13,34 @@
 #include <Adafruit_Sensor.h>
 #include <Wire.h>
 
-Adafruit_MPU6050 parent_mpu;
-Adafruit_MPU6050_Accelerometer child_mpu = Adafruit_MPU6050_Accelerometer(&parent_mpu);
-Adafruit_Sensor *mpu_temp;
+//https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050.html
+Adafruit_MPU6050 mpu;
+Adafruit_Sensor *accel;
+
+sensors_event_t event;
 
 void setup() { // initialize the buttons' inputs:
   Mouse.begin();
   Wire.begin();
   Serial.begin(9600);
 
+  Serial.println("Min value for accelerometer");
+  Serial.println(5);
+
   Serial.println("Adafruit MPU6050 test!");
    // Try to initialize!
-  while (!parent_mpu.begin()) {
+  while (!mpu.begin()) {
     Serial.println("Failed to find MPU6050 chip");
     delay(10);
   }
   Serial.println("MPU6050 Found!");
 
-  parent_mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
+  mpu.setAccelerometerRange(MPU6050_RANGE_2_G);
   Serial.print("Accelerometer range set to: +-2G");
   
-  parent_mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+  mpu.setFilterBandwidth(MPU6050_BAND_5_HZ);
+
+  accel = mpu.getAccelerometerSensor();
   
   Serial.println("");
   delay(100);
@@ -43,35 +50,38 @@ void setup() { // initialize the buttons' inputs:
   
 }
 
+/**
+ * @brief Transforms from an accelerometer value to -1 to 1
+ * @returns -1 
+ */
+float transformToPct(float reading){
+  return 0.0;
+}
+
 void loop() {
 //  delay(1000);
 //  Mouse.move(0, 40);
 
+  /* Get new sensor events with the readings 
+  https://github.com/adafruit/Adafruit_Sensor
+  https://adafruit.github.io/Adafruit_MPU6050/html/class_adafruit___m_p_u6050___accelerometer.html#af35d87ead6e4d5d2e11ee9f3d1bdf3dd
+  
+  */
+  
+  accel->getEvent(&event);
 
-  /* Get new sensor events with the readings */
-  sensors_event_t event;
-  child_mpu.getEvent(&event);
-
-  /* Print out the values */
+  /** 
+   *  Print out the accelerometer values from the sensors_event_t event
+   *  event.acceleration is a sensors_vec_t defined:
+   *  https://github.com/adafruit/Adafruit_Sensor/blob/master/Adafruit_Sensor.h#L73
+   *  (you can also get roll, pitch and heading from it)
+  */
   Serial.print("X: ");
   Serial.print(event.acceleration.x);
   Serial.print("\t Y: ");
   Serial.print(event.acceleration.y);
   Serial.print("\t Z: ");
   Serial.print(event.acceleration.z);
-//  Serial.print(" m/s^2");
-
-//  Serial.print("Rotation X: ");
-//  Serial.print(g.gyro.x);
-//  Serial.print(", Y: ");
-//  Serial.print(g.gyro.y);
-//  Serial.print(", Z: ");
-//  Serial.print(g.gyro.z);
-//  Serial.print(" rad/s");
-//
-//  Serial.print("Temperature: ");
-//  Serial.print(temp.temperature);
-//  Serial.print(" degC");
 
   Serial.println("");
   delay(500);
